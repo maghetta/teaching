a626ca92912bd050ed5313dd5f9541cb2cf76d65
maghetta <maghetta@users.noreply.github.com>
2023-03-20 20:49:45 +0100

    Update check_ping.sh
    Aggiunta riga che stampa il 20esimo carattere

--- ABC_terminale/check_ping.sh
+++ ABC_terminale/check_ping.sh
@@ -4,6 +4,7 @@ read -p "Enter Google's IP address:" ip_address
 
 if [ "$ip_address" == "216.58.209.36" ]; then
     echo "Congratulations! You entered the correct IP address."
+    echo "The 20th character is: C"
 else
     echo "Sorry, wrong IP address."
 fi
