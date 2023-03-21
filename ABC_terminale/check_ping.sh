4352913ba3dc003bd7a96e00526d49e21b61c4ae
maghetta <maghetta@users.noreply.github.com>
2023-03-21 12:09:36 +0100

    Update check_ping.sh
    modificato lo script perché prenda l'IP che considera corretto dall'esecuzione del comando ping

--- ABC_terminale/check_ping.sh
+++ ABC_terminale/check_ping.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-read -p "Enter Google's IP address:" ip_address
+# Ping al sito www.google.com
+# ping -c 1 www.google.com
 
-if [ "$ip_address" == "216.58.209.36" ]; then
-    echo "Congratulations! You entered the correct IP address."
-    echo "The 20th character is: C"
+# Recupera l'IP del sito
+ip_raw=$(ping -c 3 www.google.com | perl -ane 'if($_=~/\(([0-9,.]+)\)/){print "$1\n"}')
+ip=$(echo $ip_raw | awk '{print $1}' FS=" ")
+# echo "L'IP è $ip"
+
+# Chiede all'utente di inserire l'indirizzo IP
+read -p "Qual è l'indirizzo IP di google? " input_ip
+
+# Confronta l'input con l'indirizzo IP corretto e stampa un messaggio di congratulazioni se corretto
+if [ "$input_ip" = "$ip" ]; then
+  echo "Congratulations! You entered the correct IP address."
+  echo "The 20th character is: C"
 else
-    echo "Sorry, wrong IP address."
+  echo "Mi dispiace, hai inserito l'indirizzo IP sbagliato."
 fi
